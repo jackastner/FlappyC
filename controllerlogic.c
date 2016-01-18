5f9a7583e04c6d458ce1470ed31738f369dc6d6b
John Kastner <john.h.kastner@gmail.com>
2016-01-18 12:32:30 -0500

    Implemented hack to restore rendering ability that does not use SDL_Timer

--- controllerlogic.c
+++ controllerlogic.c
@@ -13,8 +13,8 @@ struct ControllerState {
 ControllerState *create_ControllerState(){
     ControllerState* controller = malloc(sizeof (ControllerState));
 
-    controller->tick_length = 1;
-    controller->refresh_rate = 1;
+    controller->tick_length = 100;
+    controller->refresh_rate = 1000;
 
     return controller;
 }
@@ -27,8 +27,10 @@ int run_controller(GameData* data,DrawConfig* config, ControllerState* controlle
   /*
    * These function pointer give me warnings out my bunghole.
    *
-  */
+   */
     Uint32 tick_callback(Uint32 interval, void *param){
+        printf("Tick\n");
+
         SDL_Event event;
         SDL_UserEvent userevent;
         
@@ -44,7 +46,7 @@ int run_controller(GameData* data,DrawConfig* config, ControllerState* controlle
         return(interval);
     }
 
-    SDL_TimerID tick_timer = SDL_AddTimer(controller->tick_length,tick_callback,NULL);
+    SDL_TimerID tick_timer = SDL_AddTimer(controller->tick_length,&tick_callback,NULL);
 
     // Uint32 refresh_callback(Uint32 interval, void *param){
     //     SDL_Event event;
@@ -65,24 +67,30 @@ int run_controller(GameData* data,DrawConfig* config, ControllerState* controlle
 
     SDL_Event event;
     while (!is_gameover(data)){
-        SDL_PollEvent (&event);
-        if(event.type == SDL_QUIT){
-          break;
-        }
 
-        switch(event.type) {
-            case SDL_USEREVENT: {
-                printf("User event\n");
-                void (*p) (void *, ...) = event.user.data1;
-                p(event.user.data2);
+        if(SDL_PollEvent (&event)){
+            if(event.type == SDL_QUIT){
                 break;
             }
-            case SDL_KEYDOWN: {
-                printf("Key down\n");
-                if(event.key.keysym.sym == SDLK_SPACE){
-                    flap_bird(data);
+
+            switch(event.type) {
+                case SDL_USEREVENT: {
+                    printf("User event\n");
+                    void (*p) (void *, ...) = event.user.data1;
+                    p(event.user.data2);
+                    break;
+                }
+                case SDL_KEYDOWN: {
+                    if(event.key.keysym.sym == SDLK_SPACE){
+                        printf("Flap\n");
+                        flap_bird(data);
+                    }
+                    break;
                 }
             }
+
+            //non-sdl-timer hack to make the game render
+            render_game(data,config);
         }
     }
 

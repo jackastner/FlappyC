032ac5e72133ffad961e87ee8a6e4d41b57fa495
John Kastner <john.h.kastner@gmail.com>
2016-01-24 12:24:10 -0500

    Completed adding error logging into drawlogic.c using SDL_LogError and SDL_GetError.

--- drawlogic.c
+++ drawlogic.c
@@ -116,11 +116,12 @@ void render_background(GameData* data, DrawConfig* config){
         config->window_height
     };
 
-    SDL_RenderCopy(config->renderer,config->background_texture,&source_rect,NULL);
+    if(SDL_RenderCopy(config->renderer,config->background_texture,&source_rect,NULL) != 0){
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,"Error Rendering background texture: %s\n",SDL_GetError());
+    }
 }
 
 void render_all_pipes(GameData* data, DrawConfig* config){
-    SDL_SetRenderDrawColor(config->renderer,0x00,0x00,0xFF,0xFF); //blue
     for(int i=0; i < get_num_pipes(data); i++){
         Pipe pipe = get_pipe(data,i);
         render_pipe(data,config,&pipe);
@@ -133,10 +134,14 @@ void render_pipe(GameData* data, DrawConfig* config, Pipe* pipe){
       * Query pipe textures for their height and width;
      */
     int pipe_top_texture_h, pipe_top_texture_w;
-    SDL_QueryTexture(config->pipe_top_texture, NULL, NULL, &pipe_top_texture_w, &pipe_top_texture_h);
+    if(SDL_QueryTexture(config->pipe_top_texture, NULL, NULL, &pipe_top_texture_w, &pipe_top_texture_h) != 0){
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,"Error on query of pipe top texture: %s\n",SDL_GetError());
+    }
 
     int pipe_texture_h, pipe_texture_w;
-    SDL_QueryTexture(config->pipe_texture, NULL, NULL, &pipe_texture_w, &pipe_texture_h);
+    if(SDL_QueryTexture(config->pipe_texture, NULL, NULL, &pipe_texture_w, &pipe_texture_h) != 0){
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,"Error on query of pipe texture: %s\n",SDL_GetError());
+    }
 
     /*
      * Render upper pipe
@@ -149,7 +154,10 @@ void render_pipe(GameData* data, DrawConfig* config, Pipe* pipe){
             pipe_texture_w,
             pipe_texture_h
         };
-        SDL_RenderCopy(config->renderer,config->pipe_texture,NULL,&segment_rect);                              
+        
+        if(SDL_RenderCopy(config->renderer,config->pipe_texture,NULL,&segment_rect) != 0){
+            SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,"Error Rendering pipe texture: %s\n",SDL_GetError());
+        }
     }
     SDL_Rect top_cap_rect = {
         scale_x_to_userspace(data,config,pipe->x) - (pipe_top_texture_w/2),
@@ -157,8 +165,9 @@ void render_pipe(GameData* data, DrawConfig* config, Pipe* pipe){
         pipe_top_texture_w,
         pipe_top_texture_h
     };
-    SDL_RenderCopyEx(config->renderer,config->pipe_top_texture,NULL,&top_cap_rect,0,NULL,SDL_FLIP_VERTICAL);
-
+    if(SDL_RenderCopyEx(config->renderer,config->pipe_top_texture,NULL,&top_cap_rect,0,NULL,SDL_FLIP_VERTICAL) != 0){
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,"Error Rendering pipe top  texture: %s\n",SDL_GetError());
+    }
 
     /*
      * Render lower pipe
@@ -169,7 +178,9 @@ void render_pipe(GameData* data, DrawConfig* config, Pipe* pipe){
         pipe_top_texture_w,
         pipe_top_texture_h
     };
-    SDL_RenderCopy(config->renderer,config->pipe_top_texture,NULL,&low_cap_rect);
+    if(SDL_RenderCopy(config->renderer,config->pipe_top_texture,NULL,&low_cap_rect) != 0){
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,"Error Rendering pipe top  texture: %s\n",SDL_GetError());
+    }
     for(i = scale_y_to_userspace(data, config, pipe->bottom_start) + pipe_top_texture_h; i < config->window_height ; i+= pipe_texture_h){
         SDL_Rect segment_rect = {
             scale_x_to_userspace(data,config,pipe->x) - (pipe_texture_w/2),
@@ -177,7 +188,9 @@ void render_pipe(GameData* data, DrawConfig* config, Pipe* pipe){
             pipe_texture_w,
             pipe_texture_h
         };
-        SDL_RenderCopy(config->renderer,config->pipe_texture,NULL,&segment_rect);
+        if(SDL_RenderCopy(config->renderer,config->pipe_texture,NULL,&segment_rect)){
+            SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,"Error Rendering pipe texture: %s\n",SDL_GetError());
+        }
     }
 }
 
@@ -189,11 +202,19 @@ void render_score(GameData* data, DrawConfig* config){
 
     SDL_Surface *text_surface;
     text_surface = TTF_RenderText_Solid(config->game_font,score_str,color);
+    if(text_surface == NULL){
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,"Error rendering text \"%s\": %s\n",score_str,TTF_GetError());
+    }
 
     SDL_Texture *text_texture = SDL_CreateTextureFromSurface(config->renderer,text_surface);
+    if(text_texture == NULL){
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,"Error creating texture for text \"%s\":%s\n",score_str,SDL_GetError());
+    }
 
     int text_width,text_height;
-    SDL_QueryTexture(text_texture,NULL,NULL,&text_width,&text_height);
+    if(SDL_QueryTexture(text_texture,NULL,NULL,&text_width,&text_height)  != 0){
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,"Error on query of text texture: %s\n",SDL_GetError());
+    } 
 
     SDL_Rect dest_rect = {
         (config->window_width - text_width) / 2,
@@ -202,7 +223,9 @@ void render_score(GameData* data, DrawConfig* config){
         text_height
     };
 
-    SDL_RenderCopy(config->renderer,text_texture,NULL,&dest_rect);
+    if(SDL_RenderCopy(config->renderer,text_texture,NULL,&dest_rect) != 0){
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,"Error Rendering text texture: %s\n",SDL_GetError());
+    }
 
     SDL_DestroyTexture(text_texture);
     SDL_FreeSurface(text_surface);
@@ -215,7 +238,9 @@ void render_bird(GameData* data, DrawConfig* config){
      * Query Bird image for it's width and heignt
      */
     int bird_w,bird_h;
-    SDL_QueryTexture(config->bird_texture, NULL, NULL, &bird_w, &bird_h);
+    if(SDL_QueryTexture(config->bird_texture, NULL, NULL, &bird_w, &bird_h) != 0){
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,"Error on query of bird texture: %s\n",SDL_GetError());
+    }
 
     /*
      * Specifiy where in the window the bird will be rendered.
@@ -227,44 +252,72 @@ void render_bird(GameData* data, DrawConfig* config){
         bird_h
     };
 
-    SDL_RenderCopy(config->renderer,config->bird_texture,NULL,&dest_rect);
+    if(SDL_RenderCopy(config->renderer,config->bird_texture,NULL,&dest_rect) != 0){
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,"Error Rendering bird texture: %s\n",SDL_GetError());
+    }
 }
 
 void render_gameover_message(GameData* data, DrawConfig* config){
     SDL_Color color={0,0,0};
+    
+    /*
+     * Create texture for and render a gameover message containing the final
+     * score.
+     */
 
     char score_str[35];
     sprintf(score_str, "You lost with a score of %d", get_score(data));
 
-
     SDL_Surface *score_surface;
     score_surface = TTF_RenderText_Solid(config->game_font,score_str,color);
-
+    if(score_surface == NULL){
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,"Error rendering text \"%s\": %s\n",score_str,TTF_GetError());
+    }
     SDL_Texture *score_texture = SDL_CreateTextureFromSurface(config->renderer,score_surface);
+    if(score_texture == NULL){
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,"Error creating texture for text \"%s\": %s\n",score_str,SDL_GetError());
+    }
 
     int score_width,score_height;
-    SDL_QueryTexture(score_texture,NULL,NULL,&score_width,&score_height);
+    if(SDL_QueryTexture(score_texture,NULL,NULL,&score_width,&score_height) != 0){
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,"Error on query of texture for text \"%s\": %s\n",score_str,SDL_GetError());
+    }
 
     SDL_Rect score_rect = {
         (config->window_width - score_width) / 2,
         config->window_height/4,
-        score_width,
+	score_width,
         score_height
     };
 
-    SDL_RenderCopy(config->renderer,score_texture,NULL,&score_rect);
+    if(SDL_RenderCopy(config->renderer,score_texture,NULL,&score_rect) != 0){
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,"Error rendering surface for text \"%s\": %s\n",score_str,SDL_GetError());
+    }
 
     SDL_DestroyTexture(score_texture);
     SDL_FreeSurface(score_surface);
 
+    /*
+     * Create texture for and rendure gamover message prompting the player
+     * to play again.
+     */
+    char* again_str = "Press Space to Play Again";
 
     SDL_Surface *again_surface;
-    again_surface = TTF_RenderText_Solid(config->game_font,"Press Space to Play Again",color);
+    again_surface = TTF_RenderText_Solid(config->game_font,again_str,color);
+    if(again_surface == NULL){
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,"Error rendering text \"%s\": %s\n",again_str,TTF_GetError());
+    }
 
     SDL_Texture *again_texture = SDL_CreateTextureFromSurface(config->renderer,again_surface);
-
+    if(again_texture == NULL){
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,"Error creating texture for text \"%s\": %s\n",again_str,SDL_GetError());
+    }
+   
     int again_width,again_height;
-    SDL_QueryTexture(again_texture,NULL,NULL,&again_width,&again_height);
+    if(SDL_QueryTexture(again_texture,NULL,NULL,&again_width,&again_height) != 0){
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,"Error on query of texture for text \"%s\": %s\n",again_str,SDL_GetError());
+    }
 
     SDL_Rect again_rect = {
         (config->window_width - again_width) / 2,
@@ -273,7 +326,9 @@ void render_gameover_message(GameData* data, DrawConfig* config){
         again_height
     };
 
-    SDL_RenderCopy(config->renderer,again_texture,NULL,&again_rect);
+    if(SDL_RenderCopy(config->renderer,again_texture,NULL,&again_rect) != 0){
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,"Error rendering surface for text \"%s\": %s\n",again_str,SDL_GetError());
+    }
 
     SDL_DestroyTexture(again_texture);
     SDL_FreeSurface(again_surface);
@@ -295,8 +350,12 @@ void render_game(GameData* data, DrawConfig* config){
     /*
      * Clear the window prior to any rendering
      */
-    SDL_SetRenderDrawColor(config->renderer, 0xFF, 0xFF, 0xFF, 0xFF);
-    SDL_RenderClear(config->renderer);
+    if(SDL_SetRenderDrawColor(config->renderer, 0xFF, 0xFF, 0xFF, 0xFF) != 0){
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,"Error setting render color to white (FF,FF,FF,FF): %s\n",SDL_GetError());
+    }
+    if(SDL_RenderClear(config->renderer) != 0){
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,"Error clearing renderer: %s\n",SDL_GetError());
+    }
 
     /*
      * Draw all componenets of the came

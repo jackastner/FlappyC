5e8a66479f6059542fbd24c9a16a2a41b7989eb5
John Kastner <john.h.kastner@gmail.com>
2019-01-10 10:26:45 -0500

    Throttle fps in main.

--- src/main.c
+++ src/main.c
@@ -4,13 +4,22 @@
 #include "drawlogic.h"
 #include "controllerlogic.h"
 
+#define MAX_FPS 30
+#define MS_TO_S 1000
+#define FRAME_INTERVAL (MS_TO_S / MAX_FPS)
+
 int main(int argc, char** argv){
     DrawConfig* config = create_DrawConfig();
     GameData* data = create_GameData();
     ControllerState* controller = create_ControllerState();
 
+    Uint32 last_frame = SDL_GetTicks();
     while(try_controller(data,controller)){
-        render_game(data,config);
+        Uint32 current_time = SDL_GetTicks();
+        if(SDL_TICKS_PASSED(current_time, last_frame + FRAME_INTERVAL)){ 
+            render_game(data,config);
+            last_frame = current_time;
+        } 
     }
 
     destroy_DrawConfig(config);
